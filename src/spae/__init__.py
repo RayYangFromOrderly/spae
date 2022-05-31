5042e999c76886d56146b8cec30b3c0417002782
RayYangFromOrderly <ray.yang@ezorderly.com>
2022-05-31 10:44:48 +0800

    add lazy option for lazy spark init

--- src/spae/__init__.py
+++ src/spae/__init__.py
@@ -8,11 +8,17 @@ class Spae:
     spae Client for AQL Compilation
     '''
 
-    def __init__(self, spark_url, db_url, db_user, db_password):
+    def __init__(self, spark_url, db_url, db_user, db_password, lazy=False):
         self.spark_url = spark_url
         self.db_url = db_url
         self.db_user = db_user
         self.db_password = db_password
+        self.lazy = lazy
+        self.spark = None
+        if not lazy:
+            self.build_session()
+
+    def build_session(self):
         self.spark = (
             SparkSession.builder.master(self.spark_url)
             .config("spark.jars", "/postgresql-42.3.5.jar")
@@ -21,6 +27,9 @@ class Spae:
         )
 
     def aggregate(self, aql):
+        if self.spark is None:
+            self.build_session()
+
         compiler = Compiler(self)
         compiler.pre_compile(aql)
         return compiler.run()
